815ba67df491edb794a1c9089861faccfadd4066
Diurno <Diurno@C08PC10.informatica.comercio>
2024-03-07 12:48:34 +0100

    Primer cambio

---
+++ src/main/java/clase/practicajuegolaberinto/PracticaJuegoLaberinto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package clase.practicajuegolaberinto;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PracticaJuegoLaberinto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
